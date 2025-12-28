88b390e7b60954f066e4fb4c98e0e1c1133c0477
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2025-12-28 12:39:01 +0000

    Create initial parser scaffold

--- haikulang_parser/src/ast.rs
+++ haikulang_parser/src/ast.rs
@@ -1,6 +1,6 @@
 use haikulang_lexer::token::Token;
 
-enum AstNode {
+pub enum AstNode {
     BinaryOp(Box<Self>, Token, Box<Self>),
     UnaryOp(Token, Box<Self>),
     Literal(Token),
--- haikulang_parser/src/parser.rs
+++ haikulang_parser/src/parser.rs
@@ -1,1 +1,16 @@
+use haikulang_lexer::lexer::{Lexer, LexerResult};
 
+pub struct Parser<'a> {
+    lexer: &'a mut Lexer<'a>,
+    current_token: LexerResult,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(lexer: &'a mut Lexer<'a>) -> Self {
+        let current_token = lexer.next_token();
+        Self {
+            lexer,
+            current_token,
+        }
+    }
+}
