a024b9c133083a6ef458cc32e7ef798705f06062
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2026-02-24 08:55:43 +0000

    Use Ariadne error reporting for lexer cli

--- haikulang_cli/src/lexer_cmd.rs
+++ haikulang_cli/src/lexer_cmd.rs
@@ -1,8 +1,11 @@
+use crate::error_reporting::AriadneErrorReporter;
 use clap::Args;
+use haikulang_parser::error::ErrorReporter;
 use haikulang_parser::lexer::token::Token;
 use haikulang_parser::lexer::token_stream::TokenStream;
 use std::fs::read_to_string;
 use std::path::PathBuf;
+use std::process::exit;
 
 #[derive(Args)]
 pub struct LexerCommand {
@@ -13,19 +16,26 @@ pub fn invoke_lexer(args: LexerCommand) {
     let path = args.file.as_path();
     let source = read_to_string(path).unwrap();
     let mut token_stream = TokenStream::new(&source);
+    let mut error_reporter = AriadneErrorReporter::new();
 
+    let mut index = 0;
     loop {
         match token_stream.current() {
             Ok(token) => {
-                println!("{}: {:?}", token.span(), token.value());
+                println!("{}: {}: {:?}", index, token.span(), token.value());
+                index += 1;
 
                 if token.value() == Token::Eof {
-                    return;
+                    break;
                 } else {
                     token_stream.advance();
                 }
             }
-            Err(err) => panic!("{:?}", err),
+            Err(err) => error_reporter.report(&err),
         }
+    }
+
+    if error_reporter.print(path.to_str().unwrap(), &source) {
+        exit(2);
     }
 }
