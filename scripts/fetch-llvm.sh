3180a3dcd8a6d3cd0705176a92e4b8d0b272da84
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2026-02-26 09:25:37 +0000

    Tweak fetch-llvm.sh to work with curl and fix for ubuntu

--- scripts/fetch-llvm.sh
+++ scripts/fetch-llvm.sh
@@ -13,23 +13,35 @@ function log() {
   printf "\033[1;33m[[%s]]::\033[0;33m%s\033[0m\n" "$(date)" "${*}" 2>/dev/null
 }
 
+function get() {
+  if exe_exists curl; then
+    curl -O "${1}" --silent --fail
+  elif exe_exists wget; then
+    wget "${1}"
+  else
+    log "Error: cannot find curl or wget on system \$PATH"
+    exit 2
+  fi
+}
+
 if exe_exists termux-info && exe_exists apt-get; then
   log "Installing llvm dependencies for termux..."
   apt-get install clang libcompiler-rt llvm llvm-tools
 
 elif exe_exists apt-get; then
   log "Installing llvm dependencies for Debian-based distributions..."
-  wget https://apt.llvm.org/llvm.sh
+  get https://apt.llvm.org/llvm.sh
   trap 'rm llvm.sh; trap - EXIT INT TERM' EXIT INT TERM
   chmod +x llvm.sh
   sudo ./llvm.sh "${preferred_llvm_version}"
-  rm llvm.sh
 
   # Point llvm-config to llvm-config-$LLVM_VERSION so Rust doesn't whinge about it
   log "Configuring symlink for llvm-config..."
   sudo ln -sf "$(command -v "llvm-config-${preferred_llvm_version}")" /usr/bin/llvm-config
 
 elif exe_exists dnf; then
+  # TODO: pull from llvm-snapshots COPR repository instead?
+  # https://copr.fedorainfracloud.org/coprs/g/fedora-llvm-team/llvm-snapshots/
   log "Installing llvm dependencies for Fedora-based distributions..."
   sudo dnf install llvm-devel clang-devel
 
